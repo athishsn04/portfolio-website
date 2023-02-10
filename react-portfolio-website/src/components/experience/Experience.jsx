906b097052f19a514157e77299b9f4cdae8bb8c8
athishsn04 <117382564+athishsn04@users.noreply.github.com>
2023-02-10 09:55:03 -0500

    ..
    ..

--- react-portfolio-website/src/components/experience/Experience.jsx
+++ react-portfolio-website/src/components/experience/Experience.jsx
@@ -3,7 +3,17 @@ import './experience.css'
 const Experience = () => {
   return (
     <section id='experience'>
-      Experience
+      <h5>Skills I have </h5>
+      <h2>My Experience</h2>
+      <div className='container experience__container'>
+        <div className="expierence__frontend">
+        <h3>Frontend Development</h3>
+        </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+        </div>
+      </div>
+
     </section>
   )
 }
