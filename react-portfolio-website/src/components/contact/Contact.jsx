a4b9335951cd70b3bd39fe526dedc484ff9efafb
athishsn04 <117382564+athishsn04@users.noreply.github.com>
2023-02-11 19:03:15 -0500

    .
    .

--- react-portfolio-website/src/components/contact/Contact.jsx
+++ react-portfolio-website/src/components/contact/Contact.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import './contact.css'
+import {AiOutlineMail} from 'react-icons/ai'
+import {AiOutlineMessage} from 'react-icons/ai'
+
 const Contact = () => {
   return (
     <section id='contact'>
-      Contact
+      <h5>Get In touch </h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          
+          
+          <article className="contact__option">
+            <AiOutlineMail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>athishsn@gmail.com</h5>
+            <a href="mailto:athishsn@gmail.com" target={'_blank'}>Send a Mail</a>
+          </article>
+
+          <article className="contact__option">
+            <AiOutlineMessage className='contact__option-icon'/>
+            <h4>Text </h4>
+            <h5>+1 201-699-3890</h5>
+            <a href="tel:+12016993890" target={'_blank'}>Text me on</a>
+          </article>
+          
+
+        </div>
+        {/* end of contact options */}
+        <form action="">
+          <input type="text" name='Name' placeholder='Your Full Name' required />
+          <input type="email" name='Email' placeholder='Your Email-ID' required/>
+          <textarea name="message"  rows="10" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Submit</button>
+        </form>
+      </div>
     </section>
   )
 }
--- react-portfolio-website/src/components/portfolio/Portfolio.jsx
+++ react-portfolio-website/src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/1480.gif'
+import IMG2 from '../../assets/bg.jpg'
+
+
+const data =[
+  {
+    id:1, 
+    image: IMG2,
+    title:'Crypto curreny dashboard',
+    github:'https://github.com/',
+    demo:'https://github.com',
+  },
+  {
+    id:2, 
+    image: IMG2,
+    title:'Crypto curreny dashboard',
+    github:'https://github.com/',
+    demo:'https://github.com',
+  },
+  {
+    id:3, 
+    image: IMG2,
+    title:'Crypto curreny dashboard',
+    github:'https://github.com/',
+    demo:'https://github.com',
+  },
+  {
+    id:4, 
+    image: IMG2,
+    title:'Crypto curreny dashboard',
+    github:'https://github.com/',
+    demo:'https://github.com',
+  }
+]
+
+
 const Portfolio = () => {
   return (
     <section id='portfolio'>
-      Portfolio
+      <h5>My Recent Projects</h5>
+      <h2>Portfolio</h2>
+      <div className="container portfolio__container">
+       {
+        data.map(({id, image, title, github,demo}) => {
+          return(
+            <article key={id}  className='portfolio__item'>
+            <div className='portfolio__item-image'>
+             <img src={image} alt='title'/>
+            </div>
+            <h3>{title}</h3>
+            <div className="portfolio__item-cta">
+            <a href={github} className='btn' target={'_blank'}> GitHub</a>
+            <a href={demo} className='btn btn-primary' target={'_blank'}>Live Demo</a>
+            </div>
+          </article>
+          )
+        })
+       }
+      </div>
     </section>
   )
 }
